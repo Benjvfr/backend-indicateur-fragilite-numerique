e4cd081e0a797713669c1d8ddf7703dca2c18ba0
Thibault <thibault.le.guillou79@gmail.com>
2020-11-05 10:42:25 +0100

    Add scripts folder

---
+++ scritps/score_acces_information_region.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+
+'''
+Accès à l'information
+'''
+file = r'xls/base-ic-couples-familles-menages-2016.xls'
+
+df = pd.read_excel(file, header=5)
+
+newdf = pd.DataFrame()
+newdf['REG'] = df['REG']
+newdf['LIBCOM'] = df['LIBCOM']
+newdf['LIBIRIS'] = df['LIBIRIS']
+newdf['C16_MENPSEUL'] = df['C16_MENPSEUL']
+newdf['C16_FAMMONO'] = df['C16_FAMMONO']
+
+# Part des ménages d’une personne / commune
+newdf['Part des ménages d’une personne / commune'] = df['C16_MENPSEUL']*100/df['C16_MEN']
+
+# Part des ménages d’une personne / région
+newdf['Part des ménages d’une personne / région'] = np.nan
+
+for i, r in (df.groupby(df['REG'])['C16_MENPSEUL'].sum()*100/df.groupby(df['REG'])['C16_MEN'].sum()).to_frame('Moyenne région').iterrows():
+    newdf['Part des ménages d’une personne / région'].loc[df['REG'] == i] = r['Moyenne région']
+
+# Points de la part des ménages d’une personne / commune
+newdf['Points de la part des ménages d’une personne / commune'] = (((newdf['Part des ménages d’une personne / commune']-newdf['Part des ménages d’une personne / région'])/newdf['Part des ménages d’une personne / région'])+1)*100
+
+'''
+Calcul des points de la part des familles monoparentales / commune
+'''
+# Part des familles monoparentales / commune
+newdf['Part des familles monoparentales / commune'] = df['C16_FAMMONO']*100/df['C16_MEN']
+
+# Part des familles monoparentales / région
+newdf['Part des familles monoparentales / région'] = np.nan
+
+for i, r in (df.groupby(df['REG'])['C16_FAMMONO'].sum()*100/df.groupby(df['REG'])['C16_MEN'].sum()).to_frame('Moyenne région').iterrows():
+    newdf['Part des familles monoparentales / région'].loc[df['REG'] == i] = r['Moyenne région']
+
+# Points de la part des familles monoparentales / commune
+newdf['Points de la part des familles monoparentales / commune'] = (((newdf['Part des familles monoparentales / commune']-newdf['Part des familles monoparentales / région'])/newdf['Part des familles monoparentales / région'])+1)*100
+
+# Somme des points
+newdf['Somme des points'] = newdf['Points de la part des ménages d’une personne / commune'] + newdf['Points de la part des familles monoparentales / commune']
+
+# Score de fragilité sur l'accès à l'information
+newdf['score_acces_information_iris'] = (newdf['Somme des points']*100)/(2*100)
+
+dftoexport = pd.DataFrame()
+dftoexport['nom_iris'] = newdf['LIBIRIS']
+dftoexport['nom_commune'] = newdf['LIBCOM']
+dftoexport['numero_region'] = newdf['REG']
+dftoexport['score_acces_information_iris'] = newdf['score_acces_information_iris']
+
+dftoexport.to_csv(r'xls/export.csv', sep=';')
