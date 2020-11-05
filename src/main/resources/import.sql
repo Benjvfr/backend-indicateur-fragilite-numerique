6eae1ece902e86268affe667df672989e91ec97c
Benjamin Valognes <hello@benjv.fr>
2020-11-05 21:04:15 +0100

    Modification du .gitignore

--- src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO indicateurnumerique VALUES (nextval('hibernate_sequence'), 'La Rochelle', 'Les Minimes', 'Nouvelle-Aquitaine', 100.0, 130.0, 100.0, 92.3, 100.0, 120.0, 52.0, 100.0);
-INSERT INTO indicateurnumerique VALUES (nextval('hibernate_sequence'), 'La Rochelle', 'Beauregard', 'Nouvelle-Aquitaine', 99.0, 120.0, 100.0, 100.5, 100.0, 120.0, 65.0, 100.0);
